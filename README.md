a620763b99b9dc52c04d551baf14c10861f55028
hibiki <takechi.hibiki1234@gmail.com>
2026-02-03 23:03:07 +0900

    feat: パスパラメータで四則演算を実装

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 #python fastAPI練習
 
-名前をパスパラメータで受け取るようにした
+クエリパラメータで2つの整数値を受け取り四則演算を実装した
+b = 0の時の0除算エラーを回避
 
 #コンテナの起動
 docker compose up -d --build
+
+#swaggerの起動
+http://localhost:8000/docs
--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
@@ -9,3 +9,16 @@ def home():
 @app.get("/hello/{name}")
 def say_hello(name:str):
     return {"message": f"こんにちは{name}さん"}
+
+@app.get("/add", description="クエリパラメータの二つの整数値を四則演算する")
+def calc_numbers(a:int = Query(..., description="整数値"), b:int = Query(..., description="整数値")):
+    add_value = a + b
+    sub_value = a - b
+    mul_value = a * b
+    div_value = a / b if b != 0 else None
+    return {
+        f"{a} + {b}": add_value,
+        f"{a} - {b}": sub_value,
+        f"{a} * {b}": mul_value,
+        f"{a} / {b}": div_value
+        }
